0218a0f907424026e1b5a06be3450e6dfa7b042d
Ignas Lapėnas <ignas@lapenas.dev>
2021-03-22 09:29:20 +0200

    add varle entry point

---
+++ src/varle/database.rs
@@ -0,0 +1,20 @@
+use crate::database::Database;
+use crate::database::Loadable;
+use crate::models::CData;
+use either::Left;
+use either::Right;
+use log::warn;
+use postgres::Client;
+use postgres::NoTls;
+use super::models::Attribute;
+use super::models::Category;
+use super::models::Image;
+use super::models::Product;
+use super::models::Root;
+
+pub fn load(db: Database) -> Root {
+    Root {
+    categories: Category::load_all(&db),
+    products: vec![],
+    }
+}

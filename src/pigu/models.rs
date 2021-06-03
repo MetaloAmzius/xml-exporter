020aed134ebbc4b225075a5d4b83d8413318c9ae
Ignas Lapėnas <ignas@lapenas.dev>
2021-06-03 10:20:48 +0300

    update pigu.lt model to match provided schema

--- src/pigu/models.rs
+++ src/pigu/models.rs
@@ -1,22 +1,78 @@
 use rust_decimal::Decimal;
-use crate::models::CData;
-use crate::models::Image;
+
+#[derive(Clone, Debug)]
+pub struct Attributes {
+    pub barcodes: Vec<String>,
+    pub supplier_code: String,
+}
+
+#[derive(Clone, Debug)]
+pub struct Colour {
+    pub images: Vec<Image>,
+    pub modifications: Vec<Modification>,
+    pub title: Option<String>,
+    pub title_ee: Option<String>,
+    pub title_lv: Option<String>,
+    pub title_ru: Option<String>,
+}
+
+#[derive(Clone, Debug)]
+pub struct Dimension {
+    pub height: Decimal,
+    pub length: Decimal,
+    pub package_barcode: Option<String>,
+    pub weight: Decimal,
+    pub width: Decimal,
+}
+
+#[derive(Clone, Debug)]
+pub struct Image {
+    pub md5: String,
+    pub url: String,
+}
+
+#[derive(Clone, Debug)]
+pub struct Modification {
+    pub attributes: Attributes,
+    pub height: Decimal,
+    pub length: Decimal,
+    pub multi_dimensions: Vec<Dimension>,
+    pub package_barcode: String,
+    pub title: Option<String>,
+    pub title_ee: Option<String>,
+    pub title_lv: Option<String>,
+    pub title_ru: Option<String>,
+    pub weight: Decimal,
+    pub width: Decimal,
+}
 
 #[derive(Clone, Debug)]
 pub struct Product {
-    pub sku: String,
-    pub barcode: String,
-    pub images: Vec<Image>,
+    pub category_id: String,
+    pub category_name: String,
+    pub colours: Vec<Colour>,
+    pub comments: Option<String>,
+    pub delivery_hours: Option<u32>,
+    pub guarantee: Option<u32>,
+    pub long_description: Option<String>,
+    pub long_description_ee: Option<String>,
+    pub long_description_lv: Option<String>,
+    pub long_description_ru: Option<String>,
+    pub properties: Option<Vec<Property>>,
     pub title: String,
-    pub modification: String,
-    pub description: CData,
-
-    pub weight: Decimal,
-    pub length: Decimal,
-    pub width: Decimal,
-    pub height: Decimal
+    pub title_ee: Option<String>,
+    pub title_lv: Option<String>,
+    pub title_ru: Option<String>,
+    pub video_youtube: Option<String>,
 }
 
+#[derive(Clone, Debug)]
+pub struct Property {
+    pub id: String,
+    pub values: Vec<String>,
+}
+
+#[derive(Clone, Debug)]
 pub struct Root {
     pub products: Vec<Product>
 }
